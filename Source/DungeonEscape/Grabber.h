c942f4065acc168293da4a66c7460754db867fe7
Don <doninthailand@gmail.com>
2016-11-07 20:11:56 +0000

    Moved to SSD.

---
+++ Source/DungeonEscape/Grabber.h
@@ -0,0 +1,27 @@
+// Copyright subculture software 2016
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DUNGEONESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+	float Reach = 100.f;
+	
+};
---
+++ Source/DungeonEscape/OpenDoor.h
@@ -0,0 +1,40 @@
+// Copyright subculture software 2016
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DUNGEONESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoor();
+	void CloseDoor();	
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	float DoorCloseDelay = 0.5f;
+	float LastDoorOpenTime;
+
+	AActor* ActorThatOpens; // Remember pawn inherits from actor
+	AActor* Owner; // The owning door
+};
